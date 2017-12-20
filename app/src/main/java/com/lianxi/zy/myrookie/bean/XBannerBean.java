fd54c4fc40f0827b9b9b2894e7b438ee5f9c1cc9
honeyshasha <31461892+honeyshasha@users.noreply.github.com>
2017-12-20 08:56:22 +0800

    gssssss

---
+++ app/src/main/java/com/lianxi/zy/myrookie/bean/XBannerBean.java
@@ -0,0 +1,52 @@
+package com.lianxi.zy.myrookie.bean;
+
+/**
+ * Created by 小傻瓜 on 2017/12/20.
+ */
+
+public class XBannerBean {
+
+    /**
+     * id : 1
+     * name : 音箱狂欢
+     * imgUrl : http://7mno4h.com2.z0.glb.qiniucdn.com/5608f3b5Nc8d90151.jpg
+     * type : 1
+     */
+
+    private int id;
+    private String name;
+    private String imgUrl;
+    private int type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
--- app/src/main/java/com/lianxi/zy/myrookie/retrofit/ApiService.java
+++ app/src/main/java/com/lianxi/zy/myrookie/retrofit/ApiService.java
@@ -1,6 +1,12 @@
 package com.lianxi.zy.myrookie.retrofit;
 
 
+import com.lianxi.zy.myrookie.bean.XBannerBean;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 /**
  * Created by ${张洋洋}
  * @author zy
@@ -10,7 +16,8 @@ public interface ApiService {
 
   public final  static   String BASE_URl = "http://112.124.22.238:8081/course_api/";
   /**
-   * jhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+   * http://112.124.22.238:8081/course_api/banner/query?type=1
    */
-
+  @GET("banner/query?")
+  Observable<XBannerBean> getData(@Path("type") String type);
 }
