cf96ca8fa1e601ed1258a47f4827c80b27983bb2
2337620283@qq.com <shaonianabin123>
2017-12-21 16:11:26 +0800

    zyy

---
+++ app/src/main/java/com/lianxi/zy/myrookie/app/MyAppApplication.java
@@ -0,0 +1,17 @@
+package com.lianxi.zy.myrookie.app;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+/**
+ * Created by 小傻瓜 on 2017/12/21.
+ */
+
+public class MyAppApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+    }
+}
