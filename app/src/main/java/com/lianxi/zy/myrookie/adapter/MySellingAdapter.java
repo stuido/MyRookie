07c9edf2e45b67452970fd034b740e4e1de23094
DMH1054850697 <1054850697@qq.com>
2017-12-22 14:10:09 +0800

    hhh

--- app/src/main/java/com/lianxi/zy/myrookie/adapter/MySellingAdapter.java
+++ app/src/main/java/com/lianxi/zy/myrookie/adapter/MySellingAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.interfaces.DraweeController;
@@ -28,6 +29,12 @@ public class MySellingAdapter extends RecyclerView.Adapter<MySellingAdapter.MySe
     @Override
     public MySellingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view=View.inflate(context,R.layout.f2_item,null);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.OnItemClick(view, (int) view.getTag());
+            }
+        });
         MySellingViewHolder holder=new MySellingViewHolder(view);
 
         return holder;
@@ -42,7 +49,14 @@ public class MySellingAdapter extends RecyclerView.Adapter<MySellingAdapter.MySe
                 .setAutoPlayAnimations(true)
                 .build();
         holder.f2_item_img.setController(controller);
+        holder.tv_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"我要加入购物车",Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        holder.itemView.setTag(position);
 
     }
 
@@ -50,15 +64,22 @@ public class MySellingAdapter extends RecyclerView.Adapter<MySellingAdapter.MySe
     public int getItemCount() {
         return bean.getList().size();
     }
-
+    onItemClickListener listener;
+    public void setOnItemClickListener(onItemClickListener listener){
+        this.listener=listener;
+    }
+    public interface onItemClickListener {
+        void OnItemClick(View view, int position);
+    }
     class MySellingViewHolder extends RecyclerView.ViewHolder{
-        TextView tv_describe,tv_price;
+        TextView tv_describe,tv_price,tv_play;
         SimpleDraweeView f2_item_img;
         public MySellingViewHolder(View itemView) {
             super(itemView);
             tv_describe=itemView.findViewById(R.id.tv_describe);
             tv_price=itemView.findViewById(R.id.tv_price);
             f2_item_img=itemView.findViewById(R.id.f2_item_img);
+            tv_play=itemView.findViewById(R.id.buy_now);
         }
     }
 }
