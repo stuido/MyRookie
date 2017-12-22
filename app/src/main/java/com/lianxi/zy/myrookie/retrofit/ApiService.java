09b22496cad18174a96c8eb97742e7bd81725568
DMH1054850697 <1054850697@qq.com>
2017-12-22 13:35:27 +0800

    hhh

--- app/src/main/java/com/lianxi/zy/myrookie/retrofit/ApiService.java
+++ app/src/main/java/com/lianxi/zy/myrookie/retrofit/ApiService.java
@@ -1,14 +1,20 @@
 package com.lianxi.zy.myrookie.retrofit;
 
 
+import com.lianxi.zy.myrookie.bean.FenLeiFatherBean;
+import com.lianxi.zy.myrookie.bean.FenLeiSonBean;
 import com.lianxi.zy.myrookie.bean.SellingBean;
 import com.lianxi.zy.myrookie.bean.ShouYeBean;
 import com.lianxi.zy.myrookie.bean.XBannerBean;
 
 import java.util.List;
+import java.util.Map;
 
 import io.reactivex.Observable;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 /**
@@ -23,14 +29,6 @@ public interface ApiService {
     /**
      * http://112.124.22.238:8081/course_api/banner/query?type=1
      */
-    @GET("banner/query")
-    Observable<List<XBannerBean>> getData(@Query("type") String type);
-
-    @GET("campaign/recommend")
-    Observable<List<ShouYeBean>> getShouData();
-
-    @GET("wares/hot")
-    Observable<SellingBean> getSellingData(@Query("curPage") int curPage, @Query("pageSize") int pageSize);
 
 
     //http://112.124.22.238:8081/course_api/category/list //一级分类
